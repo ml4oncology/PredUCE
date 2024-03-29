77100b3a4a8ed0957c3dc4257f11a0fe841456e2
KevinJCHe <kevinjc.he@gmail.com>
2024-03-29 19:33:44 -0400

    remove sessions after split date in dev cohort

--- src/prepare/split.py
+++ src/prepare/split.py
@@ -6,6 +6,8 @@ import logging
 from sklearn.model_selection import GroupShuffleSplit
 import pandas as pd
 
+from .filter import get_excluded_numbers
+
 logger = logging.getLogger(__name__)
 
 # Data splitting
@@ -18,10 +20,8 @@ def create_train_val_test_splits(
     train_data, test_data = create_temporal_cohort(data, split_date)
     # create validation set from train data (80-20 split)
     train_data, valid_data = create_random_split(train_data, test_size=0.2)
-
-    # sanity check - make sure there are no overlap of patients in the splits
-    assert(not set.intersection(set(train_data['mrn']), set(valid_data['mrn']), set(test_data['mrn'])))
     return train_data, valid_data, test_data
+
 
 def create_temporal_cohort(
     df: pd.DataFrame, 
@@ -33,6 +33,11 @@ def create_temporal_cohort(
     first_date = df['mrn'].map(first_date)
     mask = first_date <= split_date
     dev_cohort, test_cohort = df[mask].copy(), df[~mask].copy()
+
+    # remove visits in the dev_cohort that occured after split_date
+    mask = dev_cohort['treatment_date'] <= split_date
+    get_excluded_numbers(dev_cohort, mask, f' that occured after {split_date} in the development cohort')
+    dev_cohort = dev_cohort[mask]
     
     disp = lambda x: f"NSessions={len(x)}. NPatients={x.mrn.nunique()}"
     msg = f"Development Cohort: {disp(dev_cohort)}. Contains all patients whose first visit was on or before {split_date}"
