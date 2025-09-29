c64fcad156693215b5786b1a63989cc7f0592a62
KevinJCHe <kevinjc.he@gmail.com>
2025-09-29 17:56:08 -0400

    fixed tests

--- test/test_summarize.py
+++ test/test_summarize.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from preduce.shared.summarize import get_patient_characteristics
+from preduce.shared.summarize import cohort_summary
 
 
 @pytest.fixture
@@ -13,22 +13,22 @@ def sample_dataframe():
             "weight": [70.5, 71.0, 80.2, 79.5, 79.8, 75.3],
             "female": [1, 1, 0, 0, 0, 1],
             "regimen": ["A", "B", "A", "A", "B", "C"],
-            "cancer_C25": [1, 0, 0, 1, 1, 0],
+            "cancer_type": ["A", "B", "A", "A", "B", "C"],
             "target1": [0, 1, 0, 0, 1, 1],
         }
     )
 
 
-def test_patient_characteristics(sample_dataframe):
-    result = get_patient_characteristics(
+def test_cohort_summary(sample_dataframe):
+    result = cohort_summary(
         sample_dataframe,
         top_regimens=["B"],
-        top_cancers=["cancer_C25"],
+        top_cancers=["A"],
         targets=["target1"],
     )
     assert result["Age (years), Median (IQR)"] == "67 (62-70)"
     assert result["Height (cm), Median (IQR)"] == "169.6 (168.9-170.8)"
-    assert result["Female, No. (%)"] == "3 (50.0)"
-    assert result["Cancer Site Pancreas, No. (%)"] == "3 (50.0)"
+    # assert result["Female, No. (%)"] == "3 (50.0)"
+    assert result["Cancer Site A, No. (%)"] == "3 (50.0)"
     assert result["Regimen B, No. (%)"] == "2 (33.3)"
     assert result["TARGET1, No. (%)"] == "3 (50.0)"
